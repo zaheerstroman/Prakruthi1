9dced4d06cfc45545737d45b2d8bd21b7e24f5c4
abhishek493 <abhishek@aakrutisolutions.com>
2023-04-26 16:15:38 +0530

    Zaheer 0

---
+++ app/src/main/java/com/aakruti/prakruthi/ui/OTP_Verification.java
@@ -0,0 +1,16 @@
+package com.aakruti.prakruthi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aakruti.prakruthi.R;
+
+public class OTP_Verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+    }
+}
