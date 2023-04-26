495bfbd9edb9f4b78b5a7209a0c7105d4300d653
SakuraGlory <96517196+SakuraGlory@users.noreply.github.com>
2023-04-26 15:35:38 +0530

    sakura 7

--- app/src/main/java/com/aakruti/prakruthi/ui/Login.java
+++ app/src/main/java/com/aakruti/prakruthi/ui/Login.java
@@ -4,15 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aakruti.prakruthi.R;
 
 public class Login extends AppCompatActivity {
 
+    TextView register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        startActivity(new Intent(Login.this,RegistrationFrom.class));
+        getSupportActionBar().hide();
+        register = findViewById(R.id.register_an_account_login);
+        register.setOnClickListener(view -> {
+            startActivity(new Intent(Login.this,RegistrationFrom.class));
+        });
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
