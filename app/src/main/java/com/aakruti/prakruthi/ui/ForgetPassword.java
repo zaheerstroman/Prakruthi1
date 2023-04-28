ac99c699470e524b8336414aba0a81f151002156
SakuraGlory <96517196+SakuraGlory@users.noreply.github.com>
2023-04-28 13:51:59 +0530

    18 sakura

--- app/src/main/java/com/aakruti/prakruthi/ui/ForgetPassword.java
+++ app/src/main/java/com/aakruti/prakruthi/ui/ForgetPassword.java
@@ -1,6 +1,7 @@
 package com.aakruti.prakruthi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 
@@ -8,10 +9,15 @@ import com.aakruti.prakruthi.R;
 
 public class ForgetPassword extends AppCompatActivity {
 
+    AppCompatButton forget_password_backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
         getSupportActionBar().hide();
+        forget_password_backbtn.setOnClickListener(view -> {
+            super.onBackPressed();
+
+        });
     }
 }
