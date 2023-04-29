91c3a9d79775c199d1c5dc4a72b71176fa7d30be
SakuraGlory <96517196+SakuraGlory@users.noreply.github.com>
2023-04-29 16:23:36 +0530

    24  sakura

--- app/src/main/java/com/prakruthi/ui/ForgetPassword.java
+++ app/src/main/java/com/prakruthi/ui/ForgetPassword.java
@@ -17,6 +17,7 @@ public class ForgetPassword extends AppCompatActivity {
     EditText new_password,confirm_password,email_address;
 
     RelativeLayout via_sms_img,via_mail_img;
+    RelativeLayout forget_password_relativelayout,make_selection_relativelayout,otp_verification_relativelayout,new_credential_relativelayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,10 @@ public class ForgetPassword extends AppCompatActivity {
         setContentView(R.layout.activity_forget_password);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+        forget_password_relativelayout = findViewById(R.id.forget_password_email_RelativeLayout);
+        make_selection_relativelayout = findViewById(R.id.MAKE_SELECTION_RelativeLayout);
+        otp_verification_relativelayout = findViewById(R.id.Forget_Password_OTP_RelativeLayout);
+        new_credential_relativelayout = findViewById(R.id.new_credentials_password_email);
         new_password = findViewById(R.id.edittext_new_password);
         confirm_password = findViewById(R.id.edittext_confirm_password);
         email_address = findViewById(R.id.edittext_forgetpassword_email_adrs);
