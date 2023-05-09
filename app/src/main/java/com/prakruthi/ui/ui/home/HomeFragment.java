25f09370b93e4d71296aafa2561e6199830cdab3
Paradox <paradoxcodee@gmail.com>
2023-05-09 14:48:34 +0530

    Paradox 24

--- app/src/main/java/com/prakruthi/ui/ui/home/HomeFragment.java
+++ app/src/main/java/com/prakruthi/ui/ui/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.prakruthi.ui.ui.home;
 
 import static com.google.firebase.messaging.Constants.TAG;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -12,16 +13,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import com.cooltechworks.views.shimmer.ShimmerRecyclerView;
 import com.prakruthi.databinding.FragmentHomeBinding;
 import com.prakruthi.ui.Variables;
 import com.prakruthi.ui.ui.home.category.HomeCategoryRecyclerAdaptor;
 
 public class HomeFragment extends Fragment {
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 1;
     private FragmentHomeBinding binding;
 
     @Override
@@ -49,7 +50,7 @@ public class HomeFragment extends Fragment {
         if (Variables.address.isEmpty() || Variables.address.equals("null")) {
             binding.DeleverHomeLocation.setText("Choose Location");
             binding.DeleverHomeLocation.setOnClickListener(v -> {
-
+                GetPermission();
             });
         }
         else
@@ -61,6 +62,16 @@ public class HomeFragment extends Fragment {
         handler.postDelayed(() -> {
             binding.HomeCategoryRecyclerview.hideShimmerAdapter();
         },2000);
+    }
+
+    public void GetPermission()
+    {
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+        }
     }
     @Override
     public void onResume() {
