4cda0b7f59ab28d8a7bb4179d2895b3442804845
Paradox <paradoxcodee@gmail.com>
2023-05-20 11:27:08 +0530

    Paradox 40

---
+++ app/src/main/java/com/prakruthi/ui/ui/search/SearchPage.java
@@ -0,0 +1,16 @@
+package com.prakruthi.ui.ui.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.prakruthi.R;
+
+public class SearchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_page);
+    }
+}
