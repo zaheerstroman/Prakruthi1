d7d81e4f5874bc4c43f6f41d2ba09cfe280a1e45
Paradox <paradoxcodee@gmail.com>
2023-05-20 11:21:24 +0530

    Paradox 40

--- app/src/main/java/com/prakruthi/ui/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/prakruthi/ui/ui/profile/ProfileFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.prakruthi.R;
 import com.prakruthi.databinding.FragmentProfileBinding;
 import com.prakruthi.ui.Variables;
 
